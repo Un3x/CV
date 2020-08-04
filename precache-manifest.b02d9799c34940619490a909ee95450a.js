4e506a624c5b8af71888832292dc8babe41c1a39
Thomas COMES <comz.thomas@gmail.com>
2020-08-04 18:34:33 +0200

    Updates

---
+++ precache-manifest.b02d9799c34940619490a909ee95450a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d954f8f9c9358d64d1be087265b64de",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "7979f94738afcf4a8d7f",
+    "url": "/CV/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "095c039d018d2581864a",
+    "url": "/CV/static/js/2.16a53062.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CV/static/js/2.16a53062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7979f94738afcf4a8d7f",
+    "url": "/CV/static/js/main.f5db98f5.chunk.js"
+  },
+  {
+    "revision": "e4d273214469930d29bd",
+    "url": "/CV/static/js/runtime-main.4d0d12ac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CV/static/media/logo.5d5d9eef.svg"
+  }
+]);
